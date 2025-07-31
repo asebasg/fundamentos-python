7ded73bc495b3d1661e26622de9e3f2112a17389
Sebastián Ospina <asebasg07@gmail.com>
2025-07-31 15:39:59 -0500

    Laboratorio de cadenas literales de la seccion 2 finalizada

---
+++ src/seccion2/cadenas-literales.py
@@ -0,0 +1,16 @@
+print(10000)   # Imprime 10000
+print(10_000)  # Python admite separaciones de digitos con guiones bajos
+
+print(2.5) # Imprime 2.5
+print(2,5) # Imprime 2 5 con espacio de separacion, ya que se introdujo una coma
+
+print(2e8) # Imprime 200000000.0, en notación científica
+
+print("Me gusta \"Monty Python\"")
+print('Me gusta "Monty Python"')
+print('I\'m Monty Python.')
+
+print(True > False) # Imprime True, ya que True es mayor que False
+print(True < False) # Imprime False, ya que True no es menor que False
+
+print('"Estoy"''""aprendiendo""''"""Python"""') # Imprime "Estoy aprendiendo Python" con espacios por defecto
